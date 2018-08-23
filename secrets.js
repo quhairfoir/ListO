a7de2b75304af2ffc44b583e0221e6da732d8e2a
Morag St. Clair <moragstc@gmail.com>
2018-08-23 19:56:52 +0000

    removed secrets file

--- secrets.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  
-    GR_KEY: "4UGJGjue5hYwOv5gxWCjg",
-    GR_SECRET_KEY: "CV8uiwpg7iORrQtgBZIS3Rl1UlJAeCtjionO2sPg4",
-    
-    TMDB_KEY: "f2670be02f10c2fbd7107a9f58a4f7ae",
-    YELP_KEY: ""
-  };
