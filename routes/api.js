f5016e847169cf06a5895c1de4652a560f3f8d0c
Morag St. Clair <moragstc@gmail.com>
2018-08-24 15:34:10 +0000

    api call still not working -- more trial code included

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,10 @@
 "use strict";
 
 const secrets = require("../secrets");
-const request = require('request');
+const request = require("request");
 
 
 const yelpKey = secrets.YELP_KEY;
-const TMDBKey = secrets.TMDB_KEY;
-const GRKey = secrets.GR_KEY;
 
 module.exports = function makeAPIHelpers() {
   return {
@@ -15,33 +13,50 @@ module.exports = function makeAPIHelpers() {
       let newEntry = {};
 
       var options = { method: 'GET',
-      url: 'https://api.yelp.com/v3/businesses/search',
-      qs: { term: query, location: 'Montreal' },
-      headers: { 'Authorization': 'Bearer ' + yelpKey } };
-
-      const getNewEntry = new Promise((resolve, reject) => {
-
-        request(options, function (error, response, body) {
-          if (error) throw new Error(error);
-          var res = JSON.parse(body);
-          newEntry.businessName = res.businesses[0].name;
-          newEntry.address = res.businesses[0].location.display_address[0];
-          newEntry.type = res.businesses[0].categories[0].alias;
-          
-          resolve(newEntry);
-        })
-      })
-    
-    return getNewEntry.then(newEntry => newEntry)
-
-      // console.log(newEntry);
-      // return newEntry;
+        url: 'https://api.yelp.com/v3/businesses/search',
+        qs: { term: query, location: 'Montreal' },
+        headers: { 'Authorization': 'Bearer ' + yelpKey }
+      };
+      
+      request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+        const res = JSON.parse(body);
+        newEntry.businessName = res.businesses[0].name;
+        newEntry.address = res.businesses[0].location.display_address[0];
+        newEntry.type = res.businesses[0].categories[0].alias;
+      });
+      
+      console.log("This is newEntry from INSIDE the GET:", newEntry);
+      return (newEntry);
     },
 
-  callTMBD: function(query)  {
+    callTMBD: function()  {
 
     }
-  } 
+  }
 }
 
 
+
+// 
+
+  // let queryData = { user_id: 1, category_id: 4, query: "starbucks" };
+
+  
+  // return getNewEntry.then(newEntry => newEntry)
+
+    // console.log(newEntry);
+    // return newEntry;
+
+
+    // fetch version, only can get body
+    // var options = { method: 'GET',
+    //     url: 'https://api.yelp.com/v3/businesses/search?term=' + queryData.query + '%location=Montreal',
+    //     headers: { 'Authorization': 'Bearer ' + yelpKey } };
+
+     
+
+    //   fetch(('https://api.yelp.com/v3/businesses/search?term=Starbucks&location=Montreal'), options)
+    //     .then(res => newEntry = res.json())
+    //     .then(body => console.log(newEntry))
+  
--- routes/apiquery.js
+++ routes/apiquery.js
@@ -10,25 +10,45 @@ let queryData = { user_id: 1, category_id: 4, query: "starbucks" };
 module.exports = function(knex) {
   // includes a GET to each API based on
   router.post("/", (req, res) => {
-    let newEntry = {};
-
-    if (queryData.category_id === 4) {
-      // const getNewEntry = new Promise(function(resolve, reject) {
-        newEntry = api.toYelp(queryData.query)
-        console.log("This is newEntry from OUTSIDE api call:", newEntry);
-        // newEntry = api.toYelp(queryData.query);
-        // console.log(newEntry);
-        // if (!newEntry) {
-        //   const error = new Error("shit didn't work");
-        //   reject(error);
-        // } else if (newEntry) {
-          // resolve(newEntry)
-        // }
-      // });
-
-      // getNewEntry.then(newEntry => console.log(newEntry));
-      // will need: req.params.query (search text), req.params.user_id, req.params.category_id
-    }
+    api.toYelp(queryData.query);
   });
   return router;
 };
+
+
+// function getNewEntry(something) {
+//   return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//           if (!something) {
+//               reject(new Error("Can't do something without a thing"));
+//           } else {
+//               resolve(something);
+//           }
+//       }, 1000);
+//   });
+// }
+// getNewEntry(api.toYelp).then(console.log())
+
+
+
+
+
+// let newEntry = {};
+
+//     if (queryData.category_id === 4) {
+//       // const getNewEntry = new Promise(function(resolve, reject) {
+//         newEntry = api.toYelp(queryData.query)
+//         console.log("This is newEntry from OUTSIDE api call:", newEntry);
+//         // newEntry = api.toYelp(queryData.query);
+//         // console.log(newEntry);
+//         // if (!newEntry) {
+//         //   const error = new Error("shit didn't work");
+//         //   reject(error);
+//         // } else if (newEntry) {
+//           // resolve(newEntry)
+//         // }
+//       // });
+
+//       // getNewEntry.then(newEntry => console.log(newEntry));
+//       // will need: req.params.query (search text), req.params.user_id, req.params.category_id
+//     }
