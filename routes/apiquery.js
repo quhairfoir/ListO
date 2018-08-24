7a56d9818697ef3e71816cb983a97f97bfcabc3f
Morag St. Clair <moragstc@gmail.com>
2018-08-24 20:10:10 +0000

    knex add started, not functional

--- routes/apiquery.js
+++ routes/apiquery.js
@@ -4,7 +4,7 @@ const express = require("express");
 const router = express.Router();
 const api = require("./api")();
 
-let queryData = { user_id: 1, category_id: 2, query: "Wuthering Heights" };
+let queryData = { user_id: 1, category_id: 2, query: "Harry Potter" };
 
 // code will eventually funnel querys to search through separate APIs, unsure which route currently
 module.exports = function(knex) {
@@ -13,11 +13,13 @@ module.exports = function(knex) {
 
     console.log("this is api", api);
     let category_id = queryData.category_id;
+    let user_id = queryData.user_id;
     if (category_id === 4) {
       api.toYelp(queryData.query, (err, result) => {
         console.log(result);
 
-        // info to db
+      knex('todos')
+        .insert({name: result.name, description: result.description, category_id, user_id});
 
         res.status(201).send();
       });
@@ -33,7 +35,8 @@ module.exports = function(knex) {
       api.toGR(queryData.query, (err, result) => {
         console.log(result);
 
-        // info to db
+        knex('todos')
+        .insert({name: result.name, description: result.description, category_id, user_id});
 
         res.status(201).send();
       });
