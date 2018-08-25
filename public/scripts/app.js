6a131f0a2beb8484e05636360903e284f6da53e0
quhairfoir <moragstc@gmail.com>
2018-08-25 17:27:16 +0000

    tables now show todos filtered by user and category

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,19 +4,20 @@ const categories = {
   2: "To Read",
   3: "To Watch",
   4: "To Visit"
-}
+};
 
 // JQuery / AJAX functions
 $(() => {
-
   // example GET call, from project skeleton
   $.ajax({
     method: "GET",
     url: "/users"
-  }).done((users) => {
+  }).done(users => {
     console.log(users);
-    for(user of users) {
-      $("<div>").text(user.first_name).appendTo($("body"));
+    for (user of users) {
+      $("<div>")
+        .text(user.first_name)
+        .appendTo($("body"));
     }
   });
 
@@ -24,49 +25,72 @@ $(() => {
   $.ajax({
     method: "GET",
     url: "/todos"
-  }).done((todos) => {
+  }).done(todos => {
     for (todo of todos) {
-      // console.log("This is todo.category_id:", todo.category_id);
-      // console.log($('.list_header').html())
-      // console.log("This is $('.list_header').val()):", $('.list_header').val())
-      // if (todo.category_id === Number($('.list_header').val())){
-        $("<li>").text(todo.name).appendTo($(".list"));
-      };
-  // }
-  })
+      if (todo.user_id === Number($("#user_id").html())) {
+        if (todo.category_id === 1) {
+          $("<li>")
+            .text(todo.name)
+            .appendTo($(" #1 > .list"));
+        } else if (todo.category_id === 2) {
+          $("<li>")
+            .text(todo.name)
+            .appendTo($(" #2 > .list"));
+        } else if (todo.category_id === 3) {
+          $("<li>")
+            .text(todo.name)
+            .appendTo($(" #3 > .list"));
+        } else if (todo.category_id === 4) {
+          $("<li>")
+            .text(todo.name)
+            .appendTo($(" #4 > .list"));
+        }
+      }
+    }
+  });
 
   // onclick function to make category id available in DOM
   $("#read").click(function() {
-    $("<div>").attr('id', 'typeSelect').text(2).appendTo($('body'));
+    $("<div>")
+      .attr("id", "typeSelect")
+      .text(2)
+      .appendTo($("body"));
   });
 
   $("#watch").click(function() {
-    $("<div>").attr('id', 'typeSelect').text(3).appendTo($('body'));
+    $("<div>")
+      .attr("id", "typeSelect")
+      .text(3)
+      .appendTo($("body"));
   });
-  
+
   $("#buy").click(function() {
-    $("<div>").attr('id', 'typeSelect').text(1).appendTo($('body'));
+    $("<div>")
+      .attr("id", "typeSelect")
+      .text(1)
+      .appendTo($("body"));
   });
 
   $("#visit").click(function() {
-    $("<div>").attr('id', 'typeSelect').text(4).appendTo($('body'));
+    $("<div>")
+      .attr("id", "typeSelect")
+      .text(4)
+      .appendTo($("body"));
   });
 
   // makes query object and sends to API route
   $("#Submit").click(function() {
     let finalQuery = {
-      category_id: $('#typeSelect').html(),
+      category_id: $("#typeSelect").html(),
       query: document.getElementById("queryText").value,
       user_id: $("#user_id").html()
-      };
-      $.ajax({
-          method: "POST",
-          url: "/api",
-          data: finalQuery
-        }).done(() => {
-          console.log('yay')
-          })
-  })
-
-  
+    };
+    $.ajax({
+      method: "POST",
+      url: "/api",
+      data: finalQuery
+    }).done(() => {
+      console.log("yay");
+    });
+  });
 });
