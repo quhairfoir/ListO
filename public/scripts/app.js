74120b90fec3f9dbf4facacf4ec69cf69611903a
Hannah Norish <hannahjn@outlook.com>
2018-08-25 01:23:54 +0000

    jquery event handlers added to create submission object

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,4 +9,31 @@ $(() => {
   //     }
   //   }
   // });;
+// let typeValue = "";
+  $("#read").click(function() {
+    $("<div>").attr('id', 'typeSelect').text(2).appendTo($('body'));
+  });
+
+  $("#watch").click(function() {
+    $("<div>").attr('id', 'typeSelect').text(3).appendTo($('body'));
+  });
+  
+  $("#buy").click(function() {
+    $("<div>").attr('id', 'typeSelect').text(1).appendTo($('body'));
+  });
+
+  $("#visit").click(function() {
+    $("<div>").attr('id', 'typeSelect').text(4).appendTo($('body'));
+  });
+
+  $("#Submit").click(function() {
+    let finalQuery = {
+      category_id: $('#typeSelect').html(),
+      query: document.getElementById("queryText").value,
+      user_id: $("#user_id").html()
+      };
+      console.log(finalQuery);
+  })
+
+  
 });
